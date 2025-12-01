8a61d6e868d8ff4f2474ff64d051da85ded6b46b
marie-bisbo <49270534+marie-bisbo@users.noreply.github.com>
2025-12-01 19:05:59 +0000

    Initial commit

---
+++ AoC2025/day_1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	FILE* file = fopen(argv[1], "r");
+
+	if (file == NULL)
+	{
+		printf("No file found!\n");
+
+		return 0;
+	}
+
+	char line[10];
+	int currentPosition = 50;
+	int password = 0;
+
+	while ((fgets(line, sizeof(line), file)) != NULL)
+	{
+		char direction = line[0];
+		int amount = atoi(&line[1]);
+
+		switch (direction)
+		{
+		case 'R':
+		{
+			if ((currentPosition + amount) > 99)
+			{
+				// ++password;
+			}
+			currentPosition = (currentPosition + amount) % 100;
+			break;
+		}
+		case 'L':
+		{
+			if ((currentPosition - amount) < 0)
+			{
+				// ++password;
+			}
+			const int remainder = (currentPosition - amount) % 100;
+			currentPosition = remainder < 0 ? 100 + remainder : remainder;
+			break;
+		}
+		default:
+			continue;
+		}
+
+		if (currentPosition == 0)
+		{
+			++password;
+		}
+	}
+
+	printf("Password: %d\n", password);
+
+	return 0;
+}
